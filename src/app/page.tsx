6f988c77a3f7b849e5731d6a8183020b423d95e2
Antipas Ben <benantipas@gmail.com>
2025-05-22 00:58:06 +0100

    added whoarewe section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from './components/HeroSection';
+import WhoWeAreSection from './components/WhoWeAreSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <WhoWeAreSection />
     </>
   );
 }
